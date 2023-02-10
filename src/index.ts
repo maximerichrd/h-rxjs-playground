0d25a0b0dbd4c098709d42b80a4a77e02508931b
Maxime Richard <maxime.richard@fabernovel.com>
2023-02-10 11:23:07 +0100

    test ts-results

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,14 @@
-import { 
-  Observable as O,
-  map, 
-  from, 
-  catchError, 
-  of, 
-  combineLatest, 
-  throwError, 
-  switchMap,
-  concat,
-  concatAll,
-  concatMap,
-  mergeMap,
-  forkJoin,
-  Observable,
-  OperatorFunction
-} from 'rxjs';
-import got, { CancelableRequest } from "got";
-import {combine_} from "./rx-utils"
-
+import got from "got";
+import { Ok, Err, Result } from 'ts-results';
 
 type FirstServiceResponse = {
   code: number,
   description: string
 }
 
-type SecondServiceResponse = {
+type HttpCallOkResponse  = {
   sku: number,
   name: string
-}
-
-type ThirdServiceResponse = {
-  code: number,
-}
-
-type ComposedServiceResponse = {
-  skus: number[],
-}
-
-type ControllerResponse = {
-  products_count: number, 
-  product_codes: number[]
-}
-
-type ControllerErrorResponse = {
-  code: number,
-  message: string
 }
 
 class DomainError extends Error {
@@ -61,186 +25,97 @@ class DomainError extends Error {
 
 }
 
-interface Params {
-  fisrt: FirstServiceResponse;
-  second: SecondServiceResponse;
-  third: FirstServiceResponse;
-}
+const unstableHttpCall: Promise<HttpCallOkResponse> = new Promise<HttpCallOkResponse>(
+  (resolve, reject) => {
+    const v = Math.floor(Math.random() * 2)
+    const maybeTrue = Boolean(v)
+    console.log(`Will the fake unstable http call succeed ? ${v}`)
 
-// in a shared error module
-// cf https://blog.angular-university.io/rxjs-error-handling/
-const globalErrorHandler = (error: unknown) => {
-  if (error instanceof DomainError) {
-    switch (error.code) {
-      case 404 : 
-        console.error("404 Not Found Error: ", error)
-        return of({code: 404, message: error.message})
-      case 500 :
-        console.error("500 Internal Error: ", error)
-        return of({code: 500, message: error.message}) 
-      default: 
-        console.error(`${error.code} Error`, error)
-        return of({code: error.code, message: error.message})
+    if(maybeTrue) {
+      setTimeout(() => Ok(resolve({sku: 300, name: "A wonderful watch"})), 10)
+    } else {
+      setTimeout(() => Err(reject(new Error("Http Call Error"))), 10) 
     }
   }
-  else return of({code: 500, message: "500 Internal Error"})
-}
-
-// in a Service layer
-const firstService = ():O<FirstServiceResponse> => 
-  from<CancelableRequest<FirstServiceResponse>>(
-    got(`https://httpstat.us/200`).json()
-  ).pipe(
-    catchError((e) => throwError(() => new DomainError(500, "First Service error", e)))
-  )
-
-// in another Service layer
-const secondService = (data: FirstServiceResponse):O<SecondServiceResponse> => 
-  from<Promise<SecondServiceResponse>>(
-    // this Promise (fake http call) will be turned into 
-    // an Observable<SecondServiceResponse>
-    new Promise((resolve, reject) => {
-      if(!data) console.log("first service problem")
-
-      const v = Math.floor(Math.random() * 2)
-      const maybeTrue = Boolean(v)
-      console.log(v)
-
-      if(maybeTrue) {
-        setTimeout(() => resolve({sku: 300, name: "A wonderful watch"}), 10)
-      } else {
-        setTimeout(() => reject(new Error("Second Service Error")), 10) 
-      }
-    })
-  ).pipe(
-    catchError((e) => throwError(() => new DomainError(500, "Second Service error", e)))
-  )
-
-// in another Service layer
-const thirdService = ():O<ThirdServiceResponse> => 
-  from<CancelableRequest<ThirdServiceResponse>>(
-    got(`https://httpstat.us/400`).json()
-  ).pipe(
-    catchError((e) => throwError(() => new DomainError(500, "Third Service error", e)))
-  )
-
-// This service runs 3 services sequentially, in order
-// but does not stop the sequence when one of these services fails
-
-/* const composedService = ():O<ComposedServiceResponse> => 
-  combineLatest([
-    firstService(), 
-    secondService(),
-    thirdService()
-  ]).pipe(
-    map(([
-      firstServiceResponse,
-      secondServiceResponse,
-      thirdServiceResponse
-    ]) => {
-      return { skus: [
-        firstServiceResponse.code, 
-        secondServiceResponse.sku, 
-        thirdServiceResponse.code
-      ]}
-    })
-  ) */
-
-
-// This service runs 3 services sequentially, in order
-// and stops the sequence when one of these services fails
-// GOOD !
-// still problem: the nested arrays in the result
-const composedService1 = () => 
-firstService()
-.pipe(
-  switchMap(x => secondService(x).pipe(
-    switchMap(y => thirdService().pipe(
-      map(z => {
-        return { skus : [ x.code, y.sku, z.code] }
-      })
-    ))
-  ))
 )
 
+const firstService = async (): Promise<Result<FirstServiceResponse, DomainError>> => {
+    try {
+      // this url always returns status 200 OK
+      const response: FirstServiceResponse = await got(`https://httpstat.us/200`).json()
+      return Ok(response)
+    } catch (e) {
+      return Err(new DomainError(500, "First Service error", e))
+    }
+  }
 
-// Now we refacto the composedService1 above
-function bind<A, B>(
-  service: (p: A) => () => Observable<B>
-) {
-  return switchMap((x: A) => 
-    combineLatest([
-      of(x),
-      service(x)()
-    ])
-  ) 
+const secondService = async (param: {code: number}): Promise<Result<HttpCallOkResponse, DomainError>> => {
+  try {
+    // simulate a simple use of our param
+    if (!param) console.log("Simple use of our param")
+
+    return Ok<HttpCallOkResponse>(await unstableHttpCall)
+  } catch (e) {
+    return Err(new DomainError(500, "Second Service error", e))
+  }
 }
 
-const composedService = () => 
-  firstService()
-  .pipe(
-    bind((result: FirstServiceResponse) => () => secondService(result)),
-    bind(() => thirdService),
-    // result below
-    // is full of nested arrays 
-    // and this is a problem, we cannot code like this
-    map(([[first, second], third]) => ({ skus: [first.code, second.sku, third.code]}))
-)
-
-/*
-const composedServiceC = () => 
-concatMap
-
-firstService()
-.pipe(
-  switchMap(x => secondService().pipe(
-    switchMap(y => thirdService().pipe(
-      map(z => {
-        return { skus : [ x.code, y.sku, z.code] }
-      })
-    ))
-  ))
-)
-
-*/
-// Parallel calls
-// problem: even when secondService errors, the response is the thirdService error.
-/* const composedServiceF = () => 
-      forkJoin(
-        [
-          firstService(),
-          secondService(),
-          thirdService()
-        ]
-      ).pipe(
-        map(([one, two, three]) => {
-          return {skus: [one.code, two.sku, three.code]}
-        })
-      ) */
-
-/* const composedService = () =>
-  firstService().pipe(
-    mergeMap(x => secondService()),
-    mergeMap(x => thirdService()),
-  ) */
-
-// in the Controller layer
-function buildResponse(data: ComposedServiceResponse) {
-  return {products_count: data.skus.length, product_codes: data.skus}
+const thirdService = async (): Promise<Result<HttpCallOkResponse, DomainError>> => {
+  try {
+    return Ok<HttpCallOkResponse>(await unstableHttpCall)
+  } catch (e) {
+    return Err(new DomainError(500, "Second Service error", e))
+  }
 }
 
-function sendResponse<T>(data: T) {
+
+type Dependencies = {
+  firstService: typeof firstService,
+  secondService: typeof secondService,
+}
+
+type ControllerTask = {
+  sku: number
+  name: string
+  code: number
+}
+const controllerTask = async ({
+  firstService,
+  secondService 
+}: Dependencies): Promise<Result<ControllerTask, DomainError>> => {
+  try {
+    const code = (await firstService()).unwrap().code;
+
+    (await secondService({ code })).unwrap()
+
+    const {sku, name} = (await thirdService()).unwrap()
+    return Ok({ sku, name, code })
+  } catch (e) {
+    return Err(e)
+  }
+}
+
+const controller = async (deps: Dependencies) => {
+  const task: Result<ControllerTask, DomainError> = await controllerTask(deps)
+
+  return task
+    .map(t => send(buildResponse(t)))
+    .mapErr(e => send(negotiateErrorResponse(e)))
+
+}
+
+
+function buildResponse<T = Object>(data: T) {
+  return JSON.stringify(data)
+}
+
+function negotiateErrorResponse(e: DomainError) {
+  console.error("Error on route x")
+  return JSON.stringify({ code: e.code, message: e.message })
+
+}
+
+function send<T>(data: T) {
   console.log("Controller Response: ", data)
 }
-
-const controller = () => composedService().pipe(
-  map(data => buildResponse(data)),
-  catchError(globalErrorHandler) 
-).subscribe({
-  next: res => sendResponse<ControllerResponse | ControllerErrorResponse>(res)
-})
-
-controller()
-
-
 
---
+++ src/index2.ts
@@ -0,0 +1,261 @@
+import { 
+  Observable as O,
+  map, 
+  from, 
+  catchError, 
+  of, 
+  combineLatest, 
+  throwError, 
+  switchMap,
+  concat,
+  concatAll,
+  concatMap,
+  mergeMap,
+  forkJoin,
+  Observable,
+  OperatorFunction
+} from 'rxjs';
+import got, { CancelableRequest } from "got";
+import {combine_} from "./rx-utils"
+import { Ok, Err, Result } from 'ts-results';
+import { resultMap } from 'ts-results/rxjs-operators';
+
+const obs$: Observable<Result<number, Error>> = of(Ok(5), Err(new Error("error")));
+
+const greaterThanZero = obs$.pipe(
+    resultMap((number) => number > 0), // Doubles the value
+); // Has type Observable<Result<boolean, 'uh oh'>>
+
+greaterThanZero.subscribe((result) => {
+    if (result.ok) {
+        console.log('Was greater than zero: ' + result.val);
+    } else {
+        console.log('Got Error Message: ' + result.val);
+    }
+});
+
+type FirstServiceResponse = {
+  code: number,
+  description: string
+}
+
+type SecondServiceResponse = {
+  sku: number,
+  name: string
+}
+
+type ThirdServiceResponse = {
+  code: number,
+}
+
+type ComposedServiceResponse = {
+  skus: number[],
+}
+
+type ControllerResponse = {
+  products_count: number, 
+  product_codes: number[]
+}
+
+type ControllerErrorResponse = {
+  code: number,
+  message: string
+}
+
+class DomainError extends Error {
+  code: number
+  message: string
+  error: Error
+
+  constructor(code: number, message: string, error: Error) {
+    super()
+    this.code = code
+    this.message = message
+    this.error = error
+  }
+
+}
+
+interface Params {
+  fisrt: FirstServiceResponse;
+  second: SecondServiceResponse;
+  third: FirstServiceResponse;
+}
+
+// in a shared error module
+// cf https://blog.angular-university.io/rxjs-error-handling/
+const globalErrorHandler = (error: unknown) => {
+  if (error instanceof DomainError) {
+    switch (error.code) {
+      case 404 : 
+        console.error("404 Not Found Error: ", error)
+        return of({code: 404, message: error.message})
+      case 500 :
+        console.error("500 Internal Error: ", error)
+        return of({code: 500, message: error.message}) 
+      default: 
+        console.error(`${error.code} Error`, error)
+        return of({code: error.code, message: error.message})
+    }
+  }
+  else return of({code: 500, message: "500 Internal Error"})
+}
+
+// in a Service layer
+const firstService = ():O<FirstServiceResponse> => 
+  from<CancelableRequest<FirstServiceResponse>>(
+    got(`https://httpstat.us/200`).json()
+  ).pipe(
+    catchError((e) => throwError(() => new DomainError(500, "First Service error", e)))
+  )
+
+// in another Service layer
+const secondService = (data: FirstServiceResponse):O<SecondServiceResponse> => 
+  from<Promise<SecondServiceResponse>>(
+    // this Promise (fake http call) will be turned into 
+    // an Observable<SecondServiceResponse>
+    new Promise((resolve, reject) => {
+      if(!data) console.log("first service problem")
+
+      const v = Math.floor(Math.random() * 2)
+      const maybeTrue = Boolean(v)
+      console.log(v)
+
+      if(maybeTrue) {
+        setTimeout(() => resolve({sku: 300, name: "A wonderful watch"}), 10)
+      } else {
+        setTimeout(() => reject(new Error("Second Service Error")), 10) 
+      }
+    })
+  ).pipe(
+    catchError((e) => throwError(() => new DomainError(500, "Second Service error", e)))
+  )
+
+// in another Service layer
+const thirdService = ():O<ThirdServiceResponse> => 
+  from<CancelableRequest<ThirdServiceResponse>>(
+    got(`https://httpstat.us/400`).json()
+  ).pipe(
+    catchError((e) => throwError(() => new DomainError(500, "Third Service error", e)))
+  )
+
+// This service runs 3 services sequentially, in order
+// but does not stop the sequence when one of these services fails
+
+/* const composedService = ():O<ComposedServiceResponse> => 
+  combineLatest([
+    firstService(), 
+    secondService(),
+    thirdService()
+  ]).pipe(
+    map(([
+      firstServiceResponse,
+      secondServiceResponse,
+      thirdServiceResponse
+    ]) => {
+      return { skus: [
+        firstServiceResponse.code, 
+        secondServiceResponse.sku, 
+        thirdServiceResponse.code
+      ]}
+    })
+  ) */
+
+
+// This service runs 3 services sequentially, in order
+// and stops the sequence when one of these services fails
+// GOOD !
+// still problem: the nested arrays in the result
+const composedService1 = () => 
+firstService()
+.pipe(
+  switchMap(x => secondService(x).pipe(
+    switchMap(y => thirdService().pipe(
+      map(z => {
+        return { skus : [ x.code, y.sku, z.code] }
+      })
+    ))
+  ))
+)
+
+
+// Now we refacto the composedService1 above
+function bind<A, B>(
+  service: (p: A) => () => Observable<B>
+) {
+  return switchMap((x: A) => 
+    combineLatest([
+      of(x),
+      service(x)()
+    ])
+  ) 
+}
+
+const composedService = () => 
+  firstService()
+  .pipe(
+    bind((result: FirstServiceResponse) => () => secondService(result)),
+    bind(() => thirdService),
+    // result below
+    // is full of nested arrays 
+    // and this is a problem, we cannot code like this
+    map(([[first, second], third]) => ({ skus: [first.code, second.sku, third.code]}))
+)
+
+/*
+const composedServiceC = () => 
+concatMap
+
+firstService()
+.pipe(
+  switchMap(x => secondService().pipe(
+    switchMap(y => thirdService().pipe(
+      map(z => {
+        return { skus : [ x.code, y.sku, z.code] }
+      })
+    ))
+  ))
+)
+
+*/
+// Parallel calls
+// problem: even when secondService errors, the response is the thirdService error.
+/* const composedServiceF = () => 
+      forkJoin(
+        [
+          firstService(),
+          secondService(),
+          thirdService()
+        ]
+      ).pipe(
+        map(([one, two, three]) => {
+          return {skus: [one.code, two.sku, three.code]}
+        })
+      ) */
+
+/* const composedService = () =>
+  firstService().pipe(
+    mergeMap(x => secondService()),
+    mergeMap(x => thirdService()),
+  ) */
+
+// in the Controller layer
+function buildResponse(data: ComposedServiceResponse) {
+  return {products_count: data.skus.length, product_codes: data.skus}
+}
+
+function sendResponse<T>(data: T) {
+  console.log("Controller Response: ", data)
+}
+
+const controller = () => composedService().pipe(
+  map(data => buildResponse(data)),
+  catchError(globalErrorHandler) 
+).subscribe({
+  next: res => sendResponse<ControllerResponse | ControllerErrorResponse>(res)
+})
+
+controller()
+
+
+
